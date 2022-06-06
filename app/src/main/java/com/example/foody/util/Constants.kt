280db8fd103f56fb18367bc2f93bd7ef9c8892a5
YamamotoDesu <mynameisyamamotodesu@gmail.com>
2022-06-07 07:54:23 +0900

    Dagger Hilt Setup NetworkModule

---
+++ app/src/main/java/com/example/foody/util/Constants.kt
@@ -0,0 +1,11 @@
+package com.example.foody.util
+
+class Constants {
+
+    companion object {
+
+        const val BASE_URL = "https://api.spoonacular.com"
+        const val API_KEY = "00a4984d0b2f45f796172b076900e84e"
+
+    }
+}
